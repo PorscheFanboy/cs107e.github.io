5ce8384022f1312fe17ac62aa078687f89224f90
Pat Hanrahan <hanrahan@cs.stanford.edu>
2016-04-03 17:20:17 +0000

    Added -p option. Also should more robustly check for tty. commit fd0ed04294ff268c50808460fcb3c1c15926771c Author: Pat Hanrahan <hanrahan@cs.stanford.edu> Date:   Sun Apr 3 10:16:30 2016 -0700
        Added -p option. Also should more robustly check for tty.

--- guides/mac_toolchain/rpi-install.py
+++ guides/mac_toolchain/rpi-install.py
@@ -1,28 +1,76 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
+import getopt
 import serial
+import os
 from xmodem import XMODEM
 
-if len(sys.argv) == 3:
-    portname = sys.argv[1]
-    filename = sys.argv[2]
-elif len(sys.argv) == 2:
-    portname = "/dev/tty.SLAB_USBtoUART"
-    filename = sys.argv[1]
-else:
-    print("usage: %s tty bin" % sys.argv[0])
+def usage():
+    print( "usage: rpi-install.py [-p] [port] file" )
+    print( " -p   ..... print output from the Pi" )
+    print( " port ..... serial port (optional)" )
+    print( " file ..... binary file to upload" )
+
+def exists(path):
+    try:
+        os.stat(path)
+    except OSError:
+        return False
+    return True
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "p")
+except getopt.GetoptError:
+    usage()
     sys.exit(1)
+
+if len(args) < 1:
+    usage()
+    sys.exit(1)
+
+
+printflag = False
+for opt, arg in opts:
+    if opt == '-p':
+        printflag = True
+
+if len(args) == 2:
+    portname = args[0]
+    filename = args[1]
+else:
+    # common serial ports
+    portname = None
+    if portname is None:
+        name = "/dev/tty0"
+        if exists(name): 
+            portname = name
+    if portname is None:
+        name = "/dev/tty.SLAB_USBtoUART"
+        if exists(name): 
+            portname = name
+    if portname is None:
+        name = "/dev/ttyUSB0"
+        if exists(name):
+            portname = name
+    if portname is None:
+        name = "/dev/ttyUSB1"
+        if exists(name):
+            portname = name
+
+    filename = args[0]
+
 
 port = serial.Serial(port=portname, baudrate=115200)
 if not port:
     print(portname, 'not found')
     sys.exit(1)
 
-stream = open(filename, 'rb')
-if not stream:
+if not os.path.isfile(filename):
     print(filename, 'not found')
     sys.exit(1)
+
+stream = open(filename, 'rb')
 
 # problem was the timeout=0
 #   needed to wait for NAK
@@ -36,7 +84,17 @@ xmodem = XMODEM(getc, putc)
 
 status = xmodem.send(stream)
 print('sent', status)
-
 stream.close()
+
+if printflag:
+    try:
+        while True:
+            c = getc(1)
+            if c is None:
+                break
+            print(c,end='')
+    except:
+        pass
+
 sys.exit(int(not status))
 
