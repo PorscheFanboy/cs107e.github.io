2c1609f35e9633dfb30fc836b8feb50218c60411
Omar Rizwan <omar.rizwan@gmail.com>
2016-04-19 10:00:42 +0000

    libpi: Make assert.h actually work. commit 88ce3d1c2065395b1ec1647ada0fc9e30d2efd0a Author: Omar Rizwan <omar.rizwan@gmail.com> Date:   Tue Apr 19 02:50:52 2016 -0700
        libpi: Make assert.h actually work.

--- libpi/include/assert.h
+++ libpi/include/assert.h
@@ -2,6 +2,6 @@
 #define ASSERT_H
 
 #define assert(EXPR) \
-    if(!EXPR) printf("Assertion failed: file %s, line %d: " #EXPR "\n", __FILE__, __LINE__);
+    if(!(EXPR)) printf("Assertion failed: file %s, line %d: " #EXPR "\n", __FILE__, __LINE__);
 
 #endif
