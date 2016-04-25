4abf4324a63d3386bf3af28101547e37813d24a5
Pat Hanrahan <hanrahan@cs.stanford.edu>
2016-04-25 23:27:47 +0000

    CHanged .elf to .exe commit ee1fc7c960f2151952c0bab78d20a43cea4a76e3 Author: Pat Hanrahan <hanrahan@cs.stanford.edu> Date:   Mon Apr 25 16:25:12 2016 -0700
        CHanged .elf to .exe

--- lectures/Memory/code/sbrk/sbrk.c
+++ lectures/Memory/code/sbrk/sbrk.c
@@ -5,7 +5,7 @@ extern int __bss_end__;
 // Simple roundup function: works for powers of two.
 #define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
 
-static unsigned char *heap = (unsigned char *)&__bss_end__;
+static void *heap = (void *)&__bss_end__;
 
 void *sbrk(size_t n) {
     n = roundup(n, 8);
