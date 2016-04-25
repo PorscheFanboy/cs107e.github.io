6fa7a51d05b10ecd387a64d3397b52a6634140ca
Philip Levis <pal@cs.stanford.edu>
2016-04-25 06:04:38 +0000

    Redzone allocator. commit e3cd5b25b598519ba69650e0c1d49dbb8ac7c431 Author: Philip Levis <pal@cs.stanford.edu> Date:   Sun Apr 24 23:01:30 2016 -0700
        Redzone allocator.

--- libpi/include/malloc.h
+++ libpi/include/malloc.h
@@ -1,20 +1,34 @@
 #ifndef MALLOC_H
 #define MALLOC_H
 
-/*
- * Dynamic memory allocation.
- *
- * Author: Pat Hanrahan <hanrahan@cs.stanford.edu>
- *
- * Date: Feb 27, 2016
- */
-
+// It's great to be able to use NULL.
 #ifndef NULL
 #define NULL 0
 #endif
 
+// Bringing this type in from stddef.h.
 typedef unsigned long size_t;
 
-void* malloc(size_t n);
+// Simple roundup function: works for powers of two.
+#define roundup(x,n) (((x)+((n)-1))&(~((n)-1)))
+#define REDZONE_SIZE  2
+#define REDZONE_VALUE 0xDEADBEEF
 
+// The redzone allocation header.
+struct rz_header {
+  size_t alloc_size;
+  const char *file;
+  int line;
+  const char *func;
+};
+
+#ifdef REDZONE_MALLOC
+  #define malloc(n) _rzmalloc(n, __FILE__, __LINE__, __func__)
+  void *_rzmalloc(size_t n, const char *file, int line, const char *func);
+  int rz_check();
+#else
+  #define malloc(n) _malloc(n)
+  void *_malloc(size_t n);
 #endif
+
+#endif /* !MALLOC_H */
