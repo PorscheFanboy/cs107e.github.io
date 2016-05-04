84b8c07b195161826edf16685bd8675da18f3c6d
Philip Levis <pal@cs.stanford.edu>
2016-05-04 23:39:29 +0000

    Merge branch 'master' of github.com:cs107e/staff commit 6ae9cf353cd2718e123ed09abd9c41aabf035427 Merge: c5ffa8e 3363e36 Author: Philip Levis <pal@cs.stanford.edu> Date:   Wed May 4 16:28:21 2016 -0700
        Merge branch 'master' of github.com:cs107e/staff

--- _labs/lab5/code/keyboard_test/main.c
+++ _labs/lab5/code/keyboard_test/main.c
@@ -1,17 +1,12 @@
 #include "printf.h"
 #include "keyboard.h"
-#include "interrupts.h"
 
 void main(void) {
     printf_init();
     keyboard_init();
-    system_enable_interrupts();
-    printf("Test\n");
     while (1) {
         int scancode = keyboard_read_scancode();
-        if (scancode) {
-            printf("%02x\n", scancode);
-        }
+        printf("%02x\n", scancode);
     }
 }
 
--- _labs/lab5/code/keyboard_test/start.s
+++ _labs/lab5/code/keyboard_test/start.s
@@ -1,13 +1,6 @@
+.section .entry
 .globl _start
 _start:
-    mov r0, #0xD2
-    msr cpsr_c, r0
-    mov sp, #0x8000
-    mov r0, #0xD1       
-    msr cpsr_c, r0       
-    mov sp, #0x4000       
-    mov r0, #0xD3
-    msr cpsr_c, r0      
     mov sp,#0x8000000
     bl _cstart
 
