ce966e4214ceeaf25f41fed30911225bfbeeccd1
Pat Hanrahan <hanrahan@cs.stanford.edu>
2016-05-26 20:24:08 +0000

    Fixed typo. commit df9a3a168704f075662e4397f4fad61f37c5fd88 Author: Pat Hanrahan <hanrahan@cs.stanford.edu> Date:   Thu May 26 13:20:26 2016 -0700
        Fixed typo.

--- libpi/src/armtimer.c
+++ libpi/src/armtimer.c
@@ -1,78 +1,158 @@
+/* 
+ * Functions for Raspberry Pi ARM Timer.
+ * 
+ * Author: Pat Hanrahan <hanrahan@cs.stanford.edu>
+ *         Dawson Engler <engler@cs.stanford.edu>
+ * 
+ * Date: April 20, 2016
+*/
+#include "register.h"
 #include "armtimer.h"
 
-typedef volatile struct {
-    unsigned IRQ_basic_pending;
-    unsigned IRQ_pending_1;
-    unsigned IRQ_pending_2;
-    unsigned FIQ_control;
-    unsigned Enable_IRQs_1;
-    unsigned Enable_IRQs_2;
-    unsigned Enable_Basic_IRQs;
-    unsigned Disable_IRQs_1;
-    unsigned Disable_IRQs_2;
-    unsigned Disable_Basic_IRQs;
-} rpi_irq_controller_t;
+/*
+ * See BCM p 196 (unfortunately the documentation is wrong in several places)
+ *
+ * This timer has two parts: a free running counter and a count-down timer.
+ * The timer is always running in free running mode.
+ *
+ * In this library, we implement the count-down timer
+ *
+ * The timer and counter are based on the APB clock, which on the
+ * Raspberry Pi is 250 Mhz
+ * 
+ * Some errors in the documentation:
+ *  (1) the counters are either 32-bit (not 23-bit) or 16-bit
+ *  (2) the effect of pre-scale bits (3:2) in the control register is unclear.
+ */
 
-// p 196
 typedef volatile struct {
-    unsigned load;
-    unsigned value;                 // read only
+    unsigned load;                  // writing immediately loads the counter
+    unsigned value;                 // current counter value, read only
     struct {
-        unsigned : 1,
-                 counter_is_23_bit: 1,  // 1 = 23 bit, 0 = 16
-                 prescale: 2,           // 00 = clock / 1
-                 // 01 = clock/16
-                 // 10 = clock / 256
-                 // 11 = clock / 1 [undef?]
-                 : 1,
-                 enable_interrupt: 1,            // 1 = timer int enabled
-                 : 1,
-                 enable_timer: 1,
-                 run_in_debug: 1,        // 1 = run timer in debug
-                 disable_free_timer: 1,
-                 : 6,
-                 free_counter_scale: 8;    // clk/(prescale+1)
+        unsigned :1,
+        timer_is_32_bit:1,          // 0 -> 16-bit, 1 -> 32 bit
+        :3,
+        enable_timer_interrupt:1,   // 1 = timer interrupts enabled
+        :1,
+        enable_timer:1,             // 1 - timer enabled
+        run_in_debug:1,             // 1 = run timer in arm debug mode/halt
+        free_counter_enable:1,      // 1 = free running counter enabled
+        :6,
+        free_counter_prescale:8;    // prescalar for the free running counter
+                                    // clk/(prescale+1)
     } control;
-    unsigned irq_clear;             // write only
-    unsigned raw_irq;               // read only
-    unsigned masked_irq;            // read only
-    unsigned reload;
-    unsigned pre_div;               // not in real 804. [means?]
-    unsigned free_counter;          // free running counter not in 804?
-} arm_sys_timer_t;
+    unsigned irq_clear;             // clear irq, write only
+    unsigned overflow;              // irq pending & enabled, read only
+    unsigned irq;                   // timer interrupt request, read only
+    unsigned reload;                // this value is laoded when value == 0
+    unsigned prescale;              // prescaler for the timer clk/(prescale+1)
+    unsigned free_counter_value;    // free running counter value
+} armtimer_t;
 
-#define ARM_TIMER_LOD 0x2000B400
-#define ARM_TIMER_VAL 0x2000B404
-#define ARM_TIMER_CTL 0x2000B408
-#define ARM_TIMER_DIV 0x2000B41C
-#define ARM_TIMER_CNT 0x2000B420
-#define RPI_INTERRUPT_CONTROLLER_BASE   0x2000B200
+#define ARMTIMER_BASE 0x2000B400;
+volatile armtimer_t *armtimer = (void*)ARMTIMER_BASE;
 
-volatile rpi_irq_controller_t *rpi = (void*)RPI_INTERRUPT_CONTROLLER_BASE;
-#define RPI_BASIC_ARM_TIMER_IRQ         (1 << 0)
-#define RPI_ARMTIMER_BASE               0x2000B400
-volatile arm_sys_timer_t *sys_timer = (void*)RPI_ARMTIMER_BASE;
-
-void armtimer_init(void) {
-    unsigned* ctl = (unsigned*)ARM_TIMER_CTL;
-    *ctl = 0x00000000;
-    *ctl = 0x00000200;
-}
-
-unsigned armtimer_get_time(void) {
-    return *((unsigned*)ARM_TIMER_CNT);
-}
-
+/* Cute hack: compile-time assertion . */
 #define AssertNow(x) switch(1) { case (x): case 0: ; }
-void armtimer_start(unsigned interval) {
-    AssertNow(sizeof sys_timer->control == 4);
-    sys_timer->control.enable_timer = 1;
-    sys_timer->load = interval;
-    sys_timer->control.enable_interrupt = 1;
-    sys_timer->control.prescale = 0b10;
-    rpi->Enable_Basic_IRQs = 1;
+
+/*
+ * initialize armtimer
+ *
+ *  ticks is the number of usecs between overflows/interrupts
+ *
+ * timer is initialized, but not enabled. interrupts are disabled.
+ * 
+ */
+void  armtimer_init(unsigned ticks) 
+{ 
+    // make sure bit-fields are within 1 word
+	AssertNow(sizeof armtimer->control == 4);
+
+	armtimer->control.enable_timer = 0;
+	armtimer->control.enable_timer_interrupt = 0; 
+    armtimer->control.timer_is_32_bit = 1;
+
+    // setup up timer it counts once per microsecond
+    armtimer->prescale = (250-1); // 250 000 000 / 250 = 1 000 000 = 1 usec
+
+    // ticks are number of microseconds between overflows/interrupts
+	armtimer->load = ticks-1;
+    armtimer->reload = ticks-1;
 }
 
-void armtimer_clear_interrupt() {
-    sys_timer->irq_clear = 1;
+/*
+ * set timer prescalar (apbclock/divisor)
+ *
+ * the timer counts at a rate equal to the ABP clock divided by the divisor.
+ * the APB clock on the Raspberry Pi runs at 250 Mhz.  thus, if the divisor 
+ * equals 250, the timer will count once per usec.
+ *
+ * The prescale register is 10-bits
+ *
+ * The reset value of this register is 0x7D so gives a divide by 126.
+ */
+void armtimer_set_prescalar(unsigned divisor)
+{
+    armtimer->prescale = divisor-1;
 }
+
+/*
+ * set load register. 
+ *
+ *  this value is immediately loaded into the counter.
+ */
+void armtimer_set_load(unsigned ticks)
+{
+    armtimer->load = ticks-1;
+}
+
+/*
+ * set reload register. 
+ *
+ *  this value is loaded into the counter when it hits 0.
+ */
+void armtimer_set_reload(unsigned ticks)
+{
+    armtimer->reload = ticks-1;
+}
+
+unsigned armtimer_get_time(void)
+{
+    return armtimer->value;
+}
+
+void armtimer_enable(void)
+{
+	armtimer->control.enable_timer = 1;
+}
+
+void armtimer_disable(void)
+{
+	armtimer->control.enable_timer = 0;
+}
+
+void armtimer_enable_interrupt(void)
+{
+	armtimer->control.enable_timer_interrupt = 1;
+}
+
+void armtimer_disable_interrupt(void)
+{
+	armtimer->control.enable_timer_interrupt = 0;
+}
+
+unsigned armtimer_check_overflow(void)
+{
+    return armtimer->overflow;
+}
+
+unsigned armtimer_check_interrupt(void)
+{
+    return armtimer->irq;
+}
+
+void armtimer_clear_interrupt(void)
+{
+    armtimer->irq_clear = 1; 
+}
+
