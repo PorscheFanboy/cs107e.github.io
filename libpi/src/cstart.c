1ac5290b4d8aaaf4b890e4dd4b471119e1927445
Philip Levis <pal@cs.stanford.edu>
2017-05-10 21:49:18 +0000

    Update start.s for libpi and source code for Framebuffer lecture. commit 10c222456f6c28e29b10acb9fd86c5f2f4902fed Author: Philip Levis <pal@cs.stanford.edu> Date:   Wed May 10 14:46:32 2017 -0700
        Update start.s for libpi and source code for Framebuffer lecture.

--- libpi/src/cstart.c
+++ libpi/src/cstart.c
@@ -1,3 +1,11 @@
+#define GPIO_FSEL4  ((unsigned int *)0x20200010)
+#define GPIO_SET1   ((unsigned int *)0x20200020)
+#define GPIO_CLR1   ((unsigned int *)0x2020002c)
+
+// Green ACT LED is GPIO 47.
+#define SUCCESS_OUTPUT (1 << (3*7))
+#define SUCCESS_BIT    (1 << (47-32))
+
 extern int __bss_start__;
 extern int __bss_end__;
 
@@ -5,17 +13,22 @@ extern int __bss_end__;
 extern int _vectors;
 extern int _vectors_end;
 
-void main();
+extern void main();
 
+
+// Zeroes out the BSS and installs the interrupt handler
+// table before calling main.
+// Turns on the green ACT LED when done, to show successful completion.
+// This function is called from start.s.
 void _cstart() {
-    int* bss = &__bss_start__;
-    int* bss_end = &__bss_end__;
+    int *bss = &__bss_start__;
+    int *bss_end = &__bss_end__;
 
     while (bss < bss_end) {
         *bss++ = 0;
     }
 
-    /* Copy in interrupt vector vector and FIQ handler */
+    /* Copy in interrupt vector table and FIQ handler at end of table. */
     /* _vector and _vector_end are symbols defined in the interrupt
      assembly file, at the beginning and end of the vector and
      its embedded constants.*/
@@ -27,4 +40,8 @@ void _cstart() {
     }
 
     main();
+
+    // Turn the green ACT LED (GPIO 47).
+    *GPIO_FSEL4 = SUCCESS_OUTPUT;
+    *GPIO_SET1 = SUCCESS_BIT;
 }
