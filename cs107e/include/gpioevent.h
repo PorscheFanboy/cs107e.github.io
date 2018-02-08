6a1513b418f4a08681ce269b9dc9c9e567df28b6
Julie Zelenski <zelenski@stanford.edu>
2018-02-08 22:11:30 +0000

    Reorganized gpioevent module, added unit test commit ba58457640dd1c8cabfc7167af5ed50a3a764930 Author: Julie Zelenski <zelenski@stanford.edu> Date:   Thu Feb 8 14:06:02 2018 -0800
        Reorganized gpioevent module, added unit test

--- cs107e/include/gpioevent.h
+++ cs107e/include/gpioevent.h
@@ -13,112 +13,67 @@
 
 #include "gpio.h"
 
-
+enum gpio_event {
+    GPIO_DETECT_RISING_EDGE = 0,
+    GPIO_DETECT_FALLING_EDGE,
+    GPIO_DETECT_HIGH_LEVEL,
+    GPIO_DETECT_LOW_LEVEL,
+    GPIO_DETECT_ASYNC_RISING_EDGE,
+    GPIO_DETECT_ASYNC_FALLING_EDGE,
+};
 
 /*
- * Sets detection of falling edge events for GPIO pin number `pin` 
- * to `state`.
+ * Enables detection of GPIO `event` for GPIO pin number `pin`.
  *
- * @param pin      the GPIO pin number to set detection for
- * @param state    if 0 disables detection, otherwise enables
+ * If `pin` or `event` is invalid, does nothing.
+ */
+void gpio_enable_event_detection(unsigned int pin, unsigned int event);
+
+/*
+ * Disables detection of GPIO `event` for GPIO pin number `pin`.
+ *
+ * If `pin` or `event` is invalid, does nothing.
+ */
+void gpio_disable_event_detection(unsigned int pin, unsigned int event);
+
+/*
+ * Gets the state of detection of GPIO `event` for GPIO pin number `pin`.
+ * Returns 0 if event detection is not enabled for `event`,
+ * or 1 if event detection is enabled.
+ *
+ * If `pin` or `event` is invalid, returns GPIO_INVALID_REQUEST.
+ */
+unsigned int gpio_get_event_detection(unsigned int pin, unsigned int event);
+
+/*
+ * Disables detection of all GPIO events for GPIO pin number `pin`. 
  *
  * If `pin` is invalid, does nothing.
  */
-void gpio_set_detect_falling_edge(unsigned int pin, unsigned int state);
+void gpio_disable_all_event_detection(unsigned int pin);
 
 /*
- * Sets detection of rising edge events for GPIO pin number `pin` 
- * to `state`. 
- *
- * @param pin      the GPIO pin number to set detection for
- * @param state    if 0 disables detection, otherwise enables
- *
- * If `pin` is invalid, does nothing.
- */
-void gpio_set_detect_rising_edge(unsigned int pin, unsigned int state);
-
-/*
- * Sets detection of high level for GPIO pin number `pin` 
- * to `state`.
- *
- * @param pin      the GPIO pin number to set detection for
- * @param state    if 0 disables detection, otherwise enables
- *
- * If `pin` is invalid, does nothing.
- */
-void gpio_set_detect_high_level(unsigned int pin, unsigned int state);
-
-/*
- * Sets detection of low level for GPIO pin number `pin` 
- * to `state`.
- *
- * @param pin      the GPIO pin number to set detection for
- * @param state    if 0 disables detection, otherwise enables
- *
- * If `pin` is invalid, does nothing.
- */
-void gpio_set_detect_low_level(unsigned int pin, unsigned int state);
-
-/*
- * Sets detection of asynchronous falling edge events for GPIO pin 
- * number `pin` to `state`.
- *
- * @param pin      the GPIO pin number to set detection for
- * @param state    if 0 disables detection, otherwise enables
- *
- * If `pin` is invalid, does nothing.
- */
-void gpio_set_detect_async_falling_edge(unsigned int pin, unsigned int state);
-
-/*
- * Sets detection of asynchronous rising edge events for GPIO pin 
- * number `pin` to `state`.
- *
- * @param pin      the GPIO pin number to set detection for
- * @param state    if 0 disables detection, otherwise enables
- *
- * If `pin` is invalid, does nothing.
- */
-void gpio_set_detect_async_rising_edge(unsigned int pin, unsigned int state);
-
-/*
- * Disables detection of all event types for GPIO pin number `pin`. 
- *
- * @param pin      the GPIO pin number to disable detection for
- *
- * If `pin` is invalid, does nothing.
- */
-void gpio_disable_event_detection(unsigned int pin);
-
-/*
- * Returns event status for GPIO pin number `pin`. 
- * The event status is 1 if an event has been detected for this pin 
+ * Returns the event status for GPIO pin number `pin`. 
+ * The event status is 1 if an event has been detected for `pin` 
  * and the event has not been cleared, status is 0 otherwise.
- *
- * @param pin      the GPIO pin number to return event status
- * @return         the event status of the specified pin
  *
  * If `pin` is invalid, returns GPIO_INVALID_REQUEST.
  */
 unsigned int gpio_check_event(unsigned int pin);
 
 /*
- * Clears the event status for GPIO pin number `pin`. Note that if
- * the event was high or low level detect and the level is still 
- * high/low, the clear operation has no effect.
- *
- * @param pin      the GPIO pin number to clear event status
+ * Clears the event status for GPIO pin number `pin`. Note that if the
+ * event detected is high or low level and the level is still high or low,
+ * clearing the event makes no change in event status.  The event 
+ * status remains 1 as long as the high or low level is in effect.
  *
  * If `pin` is invalid, does nothing.
  */
 void gpio_clear_event(unsigned int pin);
 
 /*
- * Returns event status for GPIO pin number `pin` and clears 
+ * Returns the event status for GPIO pin number `pin` and clears the
  * event status.
- *
- * @param pin      the GPIO pin number to check and clear event status
- * @return         the event status of the specified pin before clearing
  *
  * If `pin` is invalid, does nothing and returns GPIO_INVALID_REQUEST.
  */
