6fd41463da430d320d510a6a6ca55d110f4799a1
Mark Miller <725mrm@gmail.com>
2018-02-16 19:50:01 +0000

    Corrected rpi_install_dest commit a8b3d6672b82574e05413e7c6cb1af4c0c36f24f Author: Mark Miller <725mrm@gmail.com> Date:   Fri Feb 16 11:46:04 2018 -0800
        Corrected rpi_install_dest

--- guides/update_rpi_install.sh
+++ guides/update_rpi_install.sh
@@ -1,5 +1,10 @@
+# (c) Stanford University 2018
+# Written by Mark Miller, 2/15/2018
+# Shell script to put the mac_toolchain rpi-install in the right place.
+# This assumes git has been pulled.
+
 rpi_install_source="cs107e.github.io/guides/mac_toolchain/rpi-install.py"
-rpi_install_dest="/usr/local/bin/rpi-install.py"
+rpi_install_dest="/home/rpi/bin/rpi-install.py"
 
 if [ ! -f "$rpi_install_source" ]; then
   echo "rpi-install was not found. Are you running this script from the same directory that cs107e.github.io is in?" 
