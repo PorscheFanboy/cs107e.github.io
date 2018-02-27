d96880ecdbd3524e60907c3a964d974e32f04fc8
Julie Zelenski <zelenski@stanford.edu>
2018-02-27 02:11:06 +0000

    Merge branch 'master' of https://github.com/cs107e/staff commit eaf2781e5eb00bb72f84b5d2daf7aec392bebb6e Merge: 0c47793 e5f153e Author: Julie Zelenski <zelenski@stanford.edu> Date:   Mon Feb 26 18:05:31 2018 -0800
        Merge branch 'master' of https://github.com/cs107e/staff

--- cs107e/include/armtimer.h
+++ cs107e/include/armtimer.h
@@ -6,21 +6,96 @@
  * 
  * Author: Pat Hanrahan <hanrahan@cs.stanford.edu>
  *         Dawson Engler <engler@cs.stanford.edu>
+ *         Julie Zelenski <zelenski@cs.stanford.edu>
  *    
- * Date: April 20, 2016
+ * Last updated: Feb 2018
  */ 
 
-void armtimer_init(unsigned int ticks);
+/*
+ * `armtimer_init`
+ *
+ * Initialize the armtimer peripheral which provides a
+ * "countdown" timer. The number of ticks to count is specified
+ * as the `tick_interval` argument to armtimer_init(). 
+ * Once the initialized armtimer is enabled, it will start the counter 
+ * at tick_internal and count down to zero. The function 
+ * armtimer_get_count() can be used to read the current value of the
+ * countdown timer. When the countdown hits zero, an overflow event
+ * happens. If interrupts are enabled, it will also generate an 
+ * interrupt on that event. At point of overflow/interrupt,
+ * timer will restart counting down.
+ */
+void armtimer_init(unsigned int tick_interval);
 
+/*
+ * `armtimer_enable`
+ *
+ * Enable the armtimer countdown timer.  Starts counting down.
+ */
 void armtimer_enable(void);
+
+/*
+ * `armtimer_disable`
+ *
+ * Disable the armtimer countdown timer.  Suspends counting down.
+ */
 void armtimer_disable(void);
 
+
+/*
+ * `armtimer_enable_interrupts`
+ *
+ * If interrupts are enabled, an interrupt is generated when the 
+ * countdown timer reaches zero.
+ */
 void armtimer_enable_interrupts(void);
+
+/*
+ * `armtimer_disable_interrupts`
+ *
+ * If interrupts are disabled, no interrupt is generated when the 
+ * countdown timer reaches zero.
+ */
 void armtimer_disable_interrupts(void);
 
+/*
+ * `armtimer_get_count`
+ *
+ * Returns the current value of the countdown timer. 
+ */
+unsigned int armtimer_get_count(void);
+
+/*
+ * Returns whether an overflow event has occurred (i.e. 
+ * countdown has reached zero).
+ * The return value is 1 if overflow event has occurred and 
+ * the event has not been cleared, 0 otherwise.
+ */
 unsigned int armtimer_check_overflow(void);
+
+/*
+ * Clears event status. Any previous overflow event is cleared.
+ */
+void armtimer_clear_event(void);
+
+/*
+ * Returns whether an overflow event occurred and clears the
+ * event.
+ */
+unsigned int armtimer_check_and_clear_overflow(void);
+
+/*
+ * Returns whether an interrupt was generated (i.e. overflow 
+ * event has occurred and interrupts are enabled).
+ * The return value is 1 if interrupt has occurred and 
+ * the event has not been cleared, 0 otherwise.
+ */
 unsigned int armtimer_check_interrupt(void);
-void armtimer_clear_interrupt(void);
+
+/*
+ * Returns whether an interrupt was generated and clears the
+ * event.
+ */
 unsigned int armtimer_check_and_clear_interrupt(void);
 
 #endif
